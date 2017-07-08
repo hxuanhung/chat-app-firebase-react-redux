6cabd540ab5a77092b96c1b97f2da54b5b97a8af
Hung HOANG <hxuanhung@gmail.com>
2017-07-08 14:32:28 +0200

    build to dist

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,30 +1,45 @@
 let path = require('path');
 let webpack = require('webpack');
+let WriteFilePlugin = require("write-file-webpack-plugin");
 
 const config = {
-  devtool: 'source-map',
+  devtool: "source-map",
   entry: [
-    'react-hot-loader/patch',
-    'webpack-hot-middleware/client?reload=false',
-    './src/index'
+    "react-hot-loader/patch",
+    "webpack-hot-middleware/client?reload=false",
+    "./src/index"
   ],
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/static/'
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/static/"
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
+  plugins: [new webpack.HotModuleReplacementPlugin(), new WriteFilePlugin()],
   module: {
     loaders: [
-      {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
-      {test: /(\.css)$/, loaders: ['style', 'css']},
-      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file"},
-      {test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff"},
-      {test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff"},
-      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream"},
-      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml"}
+      {
+        test: /\.js$/,
+        include: path.join(__dirname, "src"),
+        loaders: ["babel"]
+      },
+      { test: /(\.css)$/, loaders: ["style", "css"] },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
+      {
+        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=application/font-woff"
+      },
+      {
+        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=application/font-woff"
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=application/octet-stream"
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url?limit=10000&mimetype=image/svg+xml"
+      }
     ]
   }
 };
